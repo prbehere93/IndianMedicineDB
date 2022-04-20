ad16af0ab764c7a11d8f2e9c9a3cb165298e2563
hippocampus13 <saranshshukla@yahoo.co.in>
2022-04-20 13:11:01 +0530

    minor changes to the scraper code

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
+from drugs.urls import router as drugrouter
 
 urlpatterns = [
-    path('', include('drugs.urls')),
+    path('', include(drugrouter.urls)),
 ]
--- backend/drugs/urls.py
+++ backend/drugs/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .views import DrugViewSet
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 
-router = SimpleRouter()
+router = DefaultRouter()
 router.register(r'drugs', DrugViewSet)
-
-urlpatterns = router.urls
